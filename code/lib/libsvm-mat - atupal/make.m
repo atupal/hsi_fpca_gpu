d1681e9254e9cb31764452e65adbb7f5b8d4283d
atupal <atupalykl@gmail.com>
2015-06-09 14:29:24 +0000

    libsvm cuda matlab wrapper

---
+++ code/lib/libsvm-mat - atupal/make.m
@@ -0,0 +1,8 @@
+% This make.m is used under Windows
+
+mex -O -c -largeArrayDims svm.cpp
+mex -O -c -largeArrayDims svm_model_matlab.c
+mex -O -largeArrayDims svmtrain.c svm.obj svm_model_matlab.obj
+mex -O -largeArrayDims svmpredict.c svm.obj svm_model_matlab.obj
+mex -O -largeArrayDims libsvmread.c
+mex -O -largeArrayDims libsvmwrite.c
